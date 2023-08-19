75fd7382fed293a9dcb130cbdaa1fce752d4e20a
wali55 <walisantunu@gmail.com>
2023-08-19 14:55:18 +0600

    rtk query

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
+import { apiSlice } from './features/api/apiSlice';
 
 ReactDOM.createRoot(document.getElementById('root'))
   .render(
     <React.StrictMode>
-      <App />
+      <ApiProvider api={apiSlice}>
+        <App />
+      </ApiProvider>
     </React.StrictMode>
   );
